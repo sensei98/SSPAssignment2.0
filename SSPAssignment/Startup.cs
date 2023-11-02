b487f308b4be73ee67a0823b2fdd5f835ec9884b
sensei98 <schaltzchagly@gmail.com>
2023-11-02 18:19:55 +0100

    added startup.cs and nuget packages

---
+++ SSPAssignment/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SSPAssignment
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddAzureClients(clientBuilder =>
+            {
+                string storageAccountString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                clientBuilder.AddBlobServiceClient(storageAccountString);
+                clientBuilder.AddQueueServiceClient(storageAccountString);
+                clientBuilder.AddTableServiceClient(storageAccountString);
+                clientBuilder.UseCredential(new DefaultAzureCredential());
+
+                // Set up any default settings
+                // clientBuilder.ConfigureDefaults(builder.Configuration.GetSection("AzureDefaults"));
+            });
+            builder.Services.AddHttpClient();
+        }
+    }
+}
