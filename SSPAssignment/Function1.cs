8c2990d032ef63e4a0ef5e440cac09db1ab4f77f
sensei98 <schaltzchagly@gmail.com>
2023-11-02 19:47:55 +0100

    cleanup

--- SSPAssignment/Function1.cs
+++ SSPAssignment/Function1.cs
@@ -8,7 +8,7 @@ namespace SSPAssignment
     public class Function1
     {
         [FunctionName("Function1")]
-        public void Run([QueueTrigger("imagequeue", Connection = "ConnectionString")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("imagequeue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
