b5df834ad07fd05139600787a41c1d1c05d05cb9
sensei98 <schaltzchagly@gmail.com>
2023-11-08 14:46:15 +0100

    imagehelper class adde

--- SSPAssignment/ImageHelper.cs
+++ SSPAssignment/ImageHelper.cs
@@ -12,34 +12,34 @@ namespace SSPAssignment
 {
     public class ImageHelper
     {
-        //public static Stream AddTextToImage(Stream imageStream, params (string text, (float x, float y) position, int fontSize, string colorHex)[] texts)
-        //{
-        //    var memoryStream = new MemoryStream();
+        public static Stream AddTextToImage(Stream imageStream, params (string text, (float x, float y) position, int fontSize, string colorHex)[] texts)
+        {
+            var memoryStream = new MemoryStream();
 
-        //    var image = Image.Load(imageStream);
+            var image = Image.Load(imageStream);
 
-        //    image.Clone(img =>
-        //    {
-        //        var textGraphicsOptions = new TextGraphicsOptions()
-        //        {
-        //            TextOptions = {
-        //                    WrapTextWidth = image.Width-10
-        //            }
-        //        };
+            image.Clone(img =>
+            {
+                var textGraphicsOptions = new TextGraphicsOptions()
+                {
+                    TextOptions = {
+                            WrapTextWidth = image.Width-10
+                    }
+                };
 
-        //        foreach (var (text, (x, y), fontSize, colorHex) in texts)
-        //        {
-        //            var font = SystemFonts.CreateFont("Verdana", fontSize);
-        //            var color = Rgba32.ParseHex(colorHex);
+                foreach (var (text, (x, y), fontSize, colorHex) in texts)
+                {
+                    var font = SystemFonts.CreateFont("Verdana", fontSize);
+                    var color = Rgba32.ParseHex(colorHex);
 
-        //            img.DrawText(textGraphicsOptions, text, font, color, new PointF(x, y));
-        //        }
-        //    })
-        //        .SaveAsPng(memoryStream);
+                    img.DrawText(textGraphicsOptions, text, font, color, new PointF(x, y));
+                }
+            })
+                .SaveAsPng(memoryStream);
 
-        //    memoryStream.Position = 0;
+            memoryStream.Position = 0;
 
-        //    return memoryStream;
-        //}
+            return memoryStream;
+        }
     }
 }
